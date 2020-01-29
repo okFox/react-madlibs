9a02b7f4b765a728cb1e172ad4a149bec3fa02b9
okFox <smayfield@gmail.com>
2020-01-29 14:19:26 -0800

    header footer skeleton

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './Footer.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className={styles.Footer}>
+
+      </footer>
+    );
+  }
+}
+
